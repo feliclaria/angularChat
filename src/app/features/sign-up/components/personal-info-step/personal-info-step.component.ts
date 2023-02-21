42a0e5d36ee6d92d9aec372ec1a3f97ef9165a10
Felipe Clariá <felipe.claria@mi.unc.edu.ar>
2023-02-21 16:29:58 -0300

    Avatar validation

--- src/app/features/sign-up/components/personal-info-step/personal-info-step.component.ts
+++ src/app/features/sign-up/components/personal-info-step/personal-info-step.component.ts
@@ -1,10 +1,30 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription, switchMap } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { AvatarService } from 'src/app/services/avatar.service';
 import { UserService } from 'src/app/services/user.service';
+
+function invalidFileFormatValidator(types: string[]): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (!control.value) return null;
+
+    const splitFileName = control.value.split('.');
+    if (!splitFileName || splitFileName.length < 2) return { invalidFileFormat: true };
+
+    const extension = splitFileName.at(-1)!.toLowerCase();
+    const typeMatches = types.some((type) => type.toLowerCase() === extension);
+
+    return typeMatches ? null : { invalidFileFormat: true };
+  };
+}
 
 @Component({
   selector: 'app-personal-info-step',
@@ -12,12 +32,17 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: []
 })
 export class PersonalInfoStepComponent implements OnInit, OnDestroy {
+  @ViewChild('avatarInput')
+  avatarInputRef?: ElementRef;
+
   uid?: string;
 
   userSub?: Subscription;
 
+  avatarFile?: File;
+
   personalInfoForm = this.formBuilder.group({
-    avatar: [<File>{}],
+    avatar: ['', [invalidFileFormatValidator(['png', 'jpeg', 'jpg'])]],
     username: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(30)]]
   });
 
@@ -41,34 +66,38 @@ export class PersonalInfoStepComponent implements OnInit, OnDestroy {
 
   onFileChange(event: Event) {
     const file = (<HTMLInputElement>event.target).files?.[0];
-    if (!file) return;
 
-    this.personalInfoForm.controls.avatar.setValue(file);
+    this.avatarFile = file;
+    this.personalInfoForm.controls.avatar.setValue(file ? file.name : '');
+  }
+
+  onRemoveAvatarClick() {
+    this.avatarInputRef!.nativeElement.value = '';
+    this.avatarFile = undefined;
+    this.personalInfoForm.controls.avatar.setValue('');
   }
 
   onPersonalInfoSubmit() {
-    if (!this.uid) return;
-
-    if (this.personalInfoForm.value.avatar) {
-      this.avatarService
-        .uploadAvatar(this.uid, this.personalInfoForm.value.avatar)
-        .pipe(
-          switchMap(() => this.avatarService.getAvatarURL(this.uid!)),
-          switchMap((avatarURL) =>
-            this.userService.setUser({
-              uid: this.uid!,
-              photoURL: avatarURL
-            })
-          )
-        )
-        .subscribe();
-    }
-
-    this.userService
-      .setUser({
-        uid: this.uid,
-        displayName: this.personalInfoForm.value.username!
-      })
-      .subscribe(() => this.router.navigateByUrl('/home'));
+    // if (!this.uid) return;
+    // if (this.avatarFile) {
+    //   this.avatarService
+    //     .uploadAvatar(this.uid, this.avatarFile)
+    //     .pipe(
+    //       switchMap(() => this.avatarService.getAvatarURL(this.uid!)),
+    //       switchMap((avatarURL) =>
+    //         this.userService.setUser({
+    //           uid: this.uid!,
+    //           photoURL: avatarURL
+    //         })
+    //       )
+    //     )
+    //     .subscribe();
+    // }
+    // this.userService
+    //   .setUser({
+    //     uid: this.uid,
+    //     displayName: this.personalInfoForm.value.username!
+    //   })
+    //   .subscribe(() => this.router.navigateByUrl('/home'));
   }
 }
