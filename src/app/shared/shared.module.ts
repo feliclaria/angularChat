1d574e31b12b0bfc8bf24fdb92a756e2ecb8ef16
Felipe Clariá <felipe.claria@mi.unc.edu.ar>
2023-02-22 01:33:22 -0300

    Move components in shared module to their own directory

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { TextDividerComponent } from './text-divider.component';
+import { TextDividerComponent } from './components/text-divider.component';
 
 @NgModule({
   declarations: [TextDividerComponent],
